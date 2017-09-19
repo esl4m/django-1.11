826d071e2aef547df60605979f4a4a16c25c9edd
Eslam Elgamal <eslam.elgamal@ibm.com>
2017-09-19 16:38:57 +0200

    Adds app Bird2

---
+++ src/Bird2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Bird2Config(AppConfig):
+    name = 'Bird2'
--- src/djBird2/urls.py
+++ src/djBird2/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from Bird2.views import home
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home),
 ]
