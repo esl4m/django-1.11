303377bece51423c6b67e03bfd97730721e8be0c
Eslam Elgamal <eslam.elgamal@ibm.com>
2017-09-19 20:46:21 +0200

    Removes function based views

--- src/Bird2/views.py
+++ src/Bird2/views.py
@@ -1,17 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-# from django.http import HttpResponse
 from django.views import View
 
 
 # Create your views here.
-# function based view
-def home(request):
-    # return HttpResponse('hello home ..')
-    html_var = "hello !"
-    return render(request, 'home.html', {'html_var': html_var})   # response
-
-
 class HomeView(TemplateView):
     template_name = 'home.html'
     def get_context_data(self, *args, **kwargs):
