2108e69c774ceabc69453819039006e462951bbb
Eslam Elgamal <eslam.elgamal@ibm.com>
2017-10-04 19:54:36 +0200

    Adds first model

--- src/Bird2/models.py
+++ src/Bird2/models.py
@@ -1,3 +1,12 @@
+#  models.py
+#  by: Islam Diaa
+#      04 Oct 2017
+#
+
 from django.db import models
 
+
 # Create your models here.
+class Timeline(models.Model):
+    content = models.CharField(max_length=250)
+    date = models.DateTimeField(db_index=True, auto_now_add=True)
