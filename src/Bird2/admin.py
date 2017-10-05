9f000a0e29b848be7f5fcc82bb8efd19b5040f38
Eslam Elgamal <eslam.elgamal@ibm.com>
2017-10-05 20:54:35 +0200

    Adds model to admin

--- src/Bird2/admin.py
+++ src/Bird2/admin.py
@@ -1,3 +1,10 @@
+#  admin.py
+#  by: Islam Diaa
+#      04 Oct 2017
+#
+
 from django.contrib import admin
+from .models import Timeline
 
 # Register your models here.
+admin.site.register(Timeline)
