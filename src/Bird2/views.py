369aeed33b1a94448da8d6199d264aa4ac6e5dce
Eslam Elgamal <eslam.elgamal@ibm.com>
2017-10-11 22:34:45 +0200

    Edit in views

--- src/Bird2/views.py
+++ src/Bird2/views.py
@@ -6,7 +6,7 @@ from django.views import View
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'home.html'
-    
+
     def get_context_data(self, *args, **kwargs):
         context = super(HomeView, self).get_context_data(*args, **kwargs)
         html_var = "hello !"
